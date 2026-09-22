39c1c89d1b469c4418a1726559e3d1d9d9fb6e0b
Tim Bruns <117858473+numberlesstim@users.noreply.github.com>
2026-09-22 18:38:35 +0200

    fixed WPKSP:DVInfo throwing an exception when it shouldn't

--- src/AssemblyInfo.cs
+++ src/AssemblyInfo.cs
@@ -1,7 +1,7 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.3.0.0")]
+[assembly: AssemblyVersion("1.3.0.1")]
 [assembly: AssemblyTitle("kOS-WPKSP")]
 [assembly: AssemblyProduct("kOS_WPKSP")]
-[assembly: AssemblyFileVersion("1.3.0.0")]
+[assembly: AssemblyFileVersion("1.3.0.1")]
 [assembly: KSPAssemblyDependency("kOS", 1, 3, 0)]
